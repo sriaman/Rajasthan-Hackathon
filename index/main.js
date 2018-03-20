9e066b84fca4e1b15410c1306fb3533f9c027495
Aman Srivastav <30969315+sriaman@users.noreply.github.com>
2018-03-20 10:12:43 +0530

    Create main.js

---
+++ index/main.js
@@ -0,0 +1,7 @@
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("please fill out the details");
+        return false;
+    }
+   }
